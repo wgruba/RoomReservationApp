9b63a7bd5e62b99b62bcfacc53eec454d54814e2
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-01-21 18:08:24 +0100

    Created simple auth

---
+++ src/main/java/com/kgs/RoomReservationApp/Service/ServiceUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.kgs.RoomReservationApp.Service;
+
+import com.kgs.RoomReservationApp.Dao.ClientDao;
+import com.kgs.RoomReservationApp.Model.Client;
+import com.kgs.RoomReservationApp.Model.ServiceUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ServiceUserDetailsService implements UserDetailsService {
+
+  private final ClientDao clientDao;
+
+  @Override
+  public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+    Optional<Client> user = clientDao.getByMail(mail);
+
+    if (user.isEmpty()) throw new UsernameNotFoundException("Not found : " + mail);
+
+    return new ServiceUserDetails(user.get());
+  }
+}
